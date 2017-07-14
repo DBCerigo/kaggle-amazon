f828b4b81cb7e3ff5770f5173e10ddead24ead58
dbc-at ami=tree-boost <dbcerigo@gmail.com>
2017-07-14 11:46:05 +0000

    first auto-ml version

---
+++ dbc/tpot_exported_pipeline.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+from xgboost import XGBClassifier
+
+# NOTE: Make sure that the class is labeled 'class' in the data file
+tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
+features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
+training_features, testing_features, training_target, testing_target = \
+    train_test_split(features, tpot_data['class'], random_state=42)
+
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=XGBClassifier(learning_rate=0.001, max_depth=2, min_child_weight=15, nthread=1, subsample=0.5)),
+    LogisticRegression(dual=True, penalty="l2")
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
